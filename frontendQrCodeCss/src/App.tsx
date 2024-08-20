5d8337bfc386d2e41e8c520ccfa446bf4f1ed05f
Abhishek Panwar <abhishekpanwar1718@gmail.com>
2024-08-20 11:13:48 +0530

    completed qr project

---
+++ frontendQrCodeCss/src/App.tsx
@@ -0,0 +1,19 @@
+const App = () => {
+  return (
+    <section>
+      <article>
+        <div className="image-container">
+          <img src="./assets/images/image-qr-code.png" alt="Qr" />
+        </div>
+        <div className="qr-info">
+          <h2>
+            Improve your front-end skills by building projects
+          </h2>
+          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+      </article>
+    </section>
+  )
+}
+
+export default App
